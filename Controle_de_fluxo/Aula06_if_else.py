986be4e2ef198c01feaa0ef85940400b217cc0e0
Andrew Pires <andrewpir@gmail.com>
2023-11-14 13:08:40 -0300

    Python

---
+++ Controle_de_fluxo/Aula06_if_else.py
@@ -0,0 +1,33 @@
+
+
+velocidade = 10
+
+if velocidade > 110:
+    print("acima da velocidade permitida")
+    print("favor redusir a velocidade")
+elif velocidade < 55:
+    print("Por Favor Aumente a Velocidade")
+else:
+    print("velocidade OK")
+    
+print("Fim")
+
+'''
+podem ser utilizados operadores logicos
+Exemplo
+and
+or
+not
+
+ou podem ser usados multiplos operadores
+exemplo
+
+if valor >= 20 and valor < 40:
+
+é o mesmo que:
+
+if 20 <= valor < 40:
+
+
+
+'''
---
+++ Controle_de_fluxo/Aula07_For_loops.py
@@ -0,0 +1,18 @@
+# For Loops
+
+
+for numero in range(5): #range o padrão é numero id final
+    print(numero)
+
+print("agora de forma completa")
+    
+for numero in range(1 , 20 , 2): #range(Start, stop, step)
+    print(numero)
+    
+print("agora em palavras contando letras")
+
+palavra = "Google"
+
+for letra in palavra:
+    print(letra)
+    
