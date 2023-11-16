8a181eb1e965b96487b292bc054f80019de384e0
Andrew Pires <andrewpir@gmail.com>
2023-11-16 12:52:29 -0300

    Python

---
+++ 04_Estruturas_de_dados/Aula11_Listas.py
@@ -0,0 +1,37 @@
+
+
+#listas basico
+#armazenar mais de uma infoirmação em variaveis
+#manter a sequencia dos dados em variaveis
+#https://docs.python.org/3/tutorial/datastructures.html
+
+'''
+cidade1 = "Rio de Janeiro"
+cidade2 = "Porto Alegre"
+cidade3 = "Rolador"
+'''
+
+cidades = ["Rio de Janeiro", "São Paulo", "Porto Alegre", "Rolador"]
+
+
+print(cidades)
+
+print(cidades[0])
+print(cidades[1])
+print(cidades[2])
+print(cidades[3])
+
+#cidades[0] = "Brasólia"
+
+#cidades.append("Santa Rosa")
+
+#cidades.remove("São Paulo")
+
+#cidades.insert(1, "Brasólias")
+
+#cidades.pop(0)
+
+cidades.sort()
+
+
+print(cidades)
